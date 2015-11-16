84697564628e08f8d24163011ea3b1b50cdf29fc
raveen <raveen.rrn@gmail.com>
2015-11-16 00:36:42 -0800

    added new sysout

--- src/bread/demo/breadcls2.java
+++ src/bread/demo/breadcls2.java
@@ -5,6 +5,9 @@ public class breadcls2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("bread products");
+System.out.println("in local branch bread1");
+System.out.println("new change on bread1 repository");
+
 	}
 
 }
